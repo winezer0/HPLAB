c6a813070146dcffca5629932d882a008584470d
WINEZERO <kissloo@foxmail.com>
2023-05-24 13:25:00 +0800

    1111111

--- dict_pass/高频数字/键盘数字/从左到右/键盘数字123456.生成.py
+++ dict_pass/高频数字/键盘数字/从左到右/键盘数字123456.生成.py
@@ -26,16 +26,21 @@ def get_keyboard_x2d(keyboard_rule):
     return keyboard_rules_x2d
 
 
-def gen_key_list(keyboard_x2d, len_list=[]):
+def gen_key_list(keyboard_x2d, len_list=[], counts=[3]):
     # 1、生成 qwer 这样的键盘字符串
     line_result = []
     for length in len_list:
         for index, lst in enumerate(keyboard_x2d):
             if len(keyboard_x2d) > index + 1:
-                a1 = keyboard_x2d[index][:length]
-                a2 = keyboard_x2d[index + 1][:length]
-                string = "".join(a1) + "".join(a2)
-                line_result.append(string)
+                # a1 = keyboard_x2d[index][:length]
+                # a2 = keyboard_x2d[index + 1][:length]
+                # string = "".join(a1) + "".join(a2)
+                for count in counts:
+                    string = ""
+                    if index + count<= len(keyboard_x2d):
+                        for i in range(count):
+                            string += "".join(keyboard_x2d[index+i][:length])
+                        line_result.append(string)
     line_result = list(set(line_result))
     return line_result
 
@@ -59,14 +64,16 @@ if __name__ == '__main__':
     base_dict = {
         f"{script_name}.max.txt": {
             "length": [2, 3, 4, 5],
+            "counts": [2, 3],
         },
         f"{script_name}.min.txt": {
             "length": [3],
+            "counts": [2,3],
         },
     }
 
     for file_path, options in base_dict.items():
-        data_list = gen_key_list(keyboard_x2d, options["length"])
+        data_list = gen_key_list(keyboard_x2d, options["length"],options["counts"])
         # 仅包含纯字母选项
         data_list = [data for data in data_list if '-' not in str(data)]
         print(data_list)
